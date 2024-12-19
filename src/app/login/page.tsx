6b802f1d3379f39b5c8e1470837e3d503441f378
Igor <leckomtsevigor@yandex.ru>
2024-12-19 11:08:40 +0300

    removed second header and footer form login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,18 @@
 import Image from "next/image";
-import Footer from "~/widgets/footer/Footer";
-import Header from "~/widgets/header/Header";
 import LoginCard from "~/widgets/login/Card/LoginCard";
 
 export default function Page() {
   return (
-    <>
-      <Header />
-      <section className="flex justify-center">
-        <div className="relative max-w-xl w-full h-[683px]">
-          <Image
-            src="/login/ukraine-hotel.png"
-            fill
-            alt=""
-            className="object-cover"
-          />
-          <LoginCard className="absolute right-20 top-1/2 -translate-y-1/2 max-w-[561px]" />
-        </div>
-      </section>
-      <Footer className="!pt-12" />
-    </>
+    <section className="flex justify-center">
+      <div className="relative max-w-xl w-full h-[683px]">
+        <Image
+          src="/login/ukraine-hotel.png"
+          fill
+          alt=""
+          className="object-cover"
+        />
+        <LoginCard className="absolute right-20 top-1/2 -translate-y-1/2 max-w-[561px]" />
+      </div>
+    </section>
   );
 }
